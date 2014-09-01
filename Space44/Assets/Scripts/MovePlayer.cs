261463794b8966457df3e43b0730f1e1abf1c55c
felipeteofilo <felipe.teofilo@hotmail.com>
2014-09-01 14:50:14 -0300

    Terminado controle da nave

---
+++ Space44/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class MovePlayer : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal =Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity = movement *speed;
+
+		rigidbody.position = new Vector3
+		(
+			Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
