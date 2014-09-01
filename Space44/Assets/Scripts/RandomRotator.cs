7bfbeecd358a358c6da31728e04ae19354e22586
felipeteofilo <felipe.teofilo@hotmail.com>
2014-09-01 16:39:00 -0300

    Tiro matando asteroid
    estou gostando muito de programar em unity , esta dando tudo certo por
    enquanto

---
+++ Space44/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;
+
+	// Use this for initialization
+	void Start () {
+
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnParticleCollision(GameObject other) {
+
+		if (other.tag == "TiroBasico"){
+			Destroy (this.gameObject);
+		}
+
+	}
+}
