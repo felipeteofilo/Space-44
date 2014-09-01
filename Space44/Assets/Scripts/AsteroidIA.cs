1f540be413db4cd22140c2abfad981a02097b369
felipeteofilo <felipe.teofilo@hotmail.com>
2014-09-01 17:06:24 -0300

    Fazendo spaws de asteroids e inimigos

---
+++ Space44/Assets/Scripts/AsteroidIA.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidIA : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+
+		rigidbody.velocity = transform.forward * speed;
+	
+	}
+	
+
+}
---
+++ Space44/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void SpawnWaves(){
+		Vector3 spawnPosition = new Vector3 ();
+		Quaternion spawnRotation = new Quaternion ();
+		Instantiate (hazard, spawnPosition, spawnRotation);
+		}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
