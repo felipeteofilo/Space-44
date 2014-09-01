72d3b7f9ac023879af216a63081441dca70cc992
felipeteofilo <felipe.teofilo@hotmail.com>
2014-09-01 15:46:38 -0300

    Tiro do player funcionando

--- Space44/Assets/Scripts/PlayerController.cs
+++ Space44/Assets/Scripts/PlayerController.cs
@@ -7,37 +7,45 @@ public class Boundary
 	public float xMin, xMax, zMin, zMax;
 }
 
-public class MovePlayer : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour {
+	
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
-
+	public ParticleSystem tiro;
+	private float nextFire;
+	public float fireRate;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
-
+	
 	void FixedUpdate(){
 		float moveHorizontal =Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
-
+		
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rigidbody.velocity = movement *speed;
-
+		
 		rigidbody.position = new Vector3
-		(
-			Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
-			0.0f,
-			Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
-		);
-
+			(
+				Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+				0.0f,
+				Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+				);
+		
 		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			tiro.Emit(1);
+		}
+
 	}
 }
