e702c16977bc7829c94489ecebebeee20369a45e
felipeteofilo <felipe.teofilo@hotmail.com>
2014-09-02 13:11:54 -0300

    Terminado spawn dos inimigos
    so falta aruumar aquele bug deles aparecerem colados

---
+++ Space44/Assets/Prefabs/TurretCollision.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretCollision : MonoBehaviour {
+
+	public Transform explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+	void OnParticleCollision ( GameObject other) {
+
+		if (other.tag == "TiroBasico") {
+
+
+			Object exp = Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
+			Destroy (gameObject);
+			 
+		}
+
+	}
+
+}
--- Space44/Assets/Scripts/GameController.cs
+++ Space44/Assets/Scripts/GameController.cs
@@ -5,16 +5,38 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnWaves;
+	public int hazardCount;
+	public int hazardEnemyPerTime;
+	public float spawnWait;
+	public float startWait;
+	private float earlierX;
 	// Use this for initialization
 	void Start () {
+
+		StartCoroutine (SpawnWaves ());
 	
 	}
 
-	void SpawnWaves(){
-		Vector3 spawnPosition = new Vector3 ();
-		Quaternion spawnRotation = new Quaternion ();
-		Instantiate (hazard, spawnPosition, spawnRotation);
+	IEnumerator SpawnWaves(){
+		yield return new WaitForSeconds(startWait);
+		for (int i = 0; i < hazardCount; i++) {
+			for(int j = 0; j < hazardEnemyPerTime; j++){
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnWaves.x, spawnWaves.x), spawnWaves.y, spawnWaves.z);
+
+				while((spawnPosition.x - earlierX < 20 || spawnPosition.x - earlierX > -20) && earlierX == spawnPosition.x ){
+					spawnPosition = new Vector3 (Random.Range (-spawnWaves.x, spawnWaves.x), spawnWaves.y, spawnWaves.z);
+				}
+
+				earlierX = spawnPosition.x;
+				Instantiate (hazard, spawnPosition, hazard.transform.rotation);
+			}
+
+			if(i == hazardCount -1){
+				i=0;
+			}
+			yield return new WaitForSeconds(spawnWait);
 		}
+	}
 
 	// Update is called once per frame
 	void Update () {
