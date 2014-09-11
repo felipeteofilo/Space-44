d58b27b1644f88f3809136127c55b4f6cc1bb352
felipeteofilo <felipe.teofilo@hotmail.com>
2014-09-11 09:44:14 -0300

    Colocado botao restart
    arrumado tambem o bug da referencia

--- Space44/Assets/Scripts/GameController.cs
+++ Space44/Assets/Scripts/GameController.cs
@@ -1,89 +1,106 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameController : MonoBehaviour {
-
-	public GameObject enemy1;
-	public GameObject enemy2;
-	public GameObject boss;
-	public GameObject background;
-	public GameObject asteroid;
-	public Vector3 spawnWaves;
-
-	[Range(0,100)]
-	public int percentFirstEnemy;
-
-	[Range(0,100 )]
-	public int percentSecondEnemy;
-
-	[Range(0,100 )]
-	public int percentAsteroid;
+public class GameController : MonoBehaviour
+{
+		public GUIText restart;
+		public GameObject enemy1;
+		public GameObject enemy2;
+		public GameObject boss;
+		public GameObject background;
+		public GameObject asteroid;
+		public Vector3 spawnWaves;
+		[Range(0,100)]
+		public int
+				percentFirstEnemy;
+		[Range(0,100 )]
+		public int
+				percentSecondEnemy;
+		[Range(0,100 )]
+		public int
+				percentAsteroid;
 
 //	public int percentThirdEnemy;
 //	public int percentFourthEnemy;
-	public float spawnWait;
-	public float startWait;
-	private float earlierX;
-	private AudioSource bossSong;
+		public float spawnWait;
+		public float startWait;
+		private float earlierX;
+		private AudioSource bossSong;
+		bool bossIstantiate;
 
-	bool bossIstantiate;
 
-
-	// Use this for initialization
-	void Start () {
-		AudioSource[] audios = GetComponents<AudioSource>();
-		bossSong = audios [1];
-		StartCoroutine (SpawnWaves ());
+		// Use this for initialization
+		void Start ()
+		{
+				AudioSource[] audios = GetComponents<AudioSource> ();
+				bossSong = audios [1];
+				StartCoroutine (SpawnWaves ());
 	
-	}
-
-	IEnumerator SpawnWaves(){
-		yield return new WaitForSeconds(startWait);
-		while(true) {
-			Vector3 spawnPosition = new Vector3 (Random.Range (-spawnWaves.x, spawnWaves.x), spawnWaves.y, spawnWaves.z);
-
-			GameObject enemy = ramdomEnemy();
-			if(enemy == asteroid){
-				Instantiate (enemy, spawnPosition, enemy.transform.rotation);
-				spawnPosition = new Vector3 (Random.Range (-spawnWaves.x, spawnWaves.x), spawnWaves.y, spawnWaves.z);
-				Instantiate (enemy, spawnPosition, enemy.transform.rotation);
-				spawnPosition = new Vector3 (Random.Range (-spawnWaves.x, spawnWaves.x), spawnWaves.y, spawnWaves.z);
-			}
-			Instantiate (enemy, spawnPosition, enemy.transform.rotation);
-
-			yield return new WaitForSeconds(spawnWait);
 		}
-	}
 
-	GameObject ramdomEnemy(){
-		float percent = Random.Range(1,100);
+		IEnumerator SpawnWaves ()
+		{
+				yield return new WaitForSeconds (startWait);
+				while (true) {
+						Vector3 spawnPosition = new Vector3 (Random.Range (-spawnWaves.x, spawnWaves.x), spawnWaves.y, spawnWaves.z);
 
-		if (percent <= percentAsteroid) {
-			return asteroid;
+						GameObject enemy = ramdomEnemy ();
+						if (enemy == asteroid) {
+								Instantiate (enemy, spawnPosition, enemy.transform.rotation);
+								spawnPosition = new Vector3 (Random.Range (-spawnWaves.x, spawnWaves.x), spawnWaves.y, spawnWaves.z);
+								Instantiate (enemy, spawnPosition, enemy.transform.rotation);
+								spawnPosition = new Vector3 (Random.Range (-spawnWaves.x, spawnWaves.x), spawnWaves.y, spawnWaves.z);
+						}
+						Instantiate (enemy, spawnPosition, enemy.transform.rotation);
+
+						yield return new WaitForSeconds (spawnWait);
+				}
 		}
-		else if (percent <= percentAsteroid + percentFirstEnemy) {
-			return enemy1;
-		}
-		else if(percent <= percentAsteroid + percentFirstEnemy+ percentSecondEnemy){
-			return enemy2;
-		}
-		return null;
+
+		GameObject ramdomEnemy ()
+		{
+				float percent = Random.Range (1, 100);
+
+				if (percent <= percentAsteroid) {
+						return asteroid;
+				} else if (percent <= percentAsteroid + percentFirstEnemy) {
+						return enemy1;
+				} else if (percent <= percentAsteroid + percentFirstEnemy + percentSecondEnemy) {
+						return enemy2;
+				}
+				return null;
 				
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-		if (background.transform.localPosition.z > 2.6f) {
-						background.transform.Translate (background.transform.forward * -0.01f);
-		} else if(!bossIstantiate){
-				audio.Stop();
-				bossSong.Play();
-				StopAllCoroutines();
-				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnWaves.x, spawnWaves.x), spawnWaves.y, 30);
-				Instantiate (boss, spawnPosition, boss.transform.rotation);
-				bossIstantiate = true;
 		}
 
-	}
+		// Update is called once per frame
+		void Update ()
+		{
+				if (!GameObject.FindGameObjectWithTag ("Player")) {
+						if (!restart.gameObject.activeSelf) {
+								restart.gameObject.SetActive (true);
+						}
+						if (Input.GetKeyDown (KeyCode.R)) {
+								Application.LoadLevel (Application.loadedLevel);
+
+						}
+
+				}
+
+				if (background.transform.localPosition.z > 2.6f) {
+						background.transform.Translate (background.transform.forward * -0.01f);
+				} else if (!bossIstantiate) {
+						audio.Stop ();
+						bossSong.Play ();
+						StopAllCoroutines ();
+						ParticleSystem[] stars = GameObject.FindGameObjectWithTag ("Stars").GetComponentsInChildren<ParticleSystem> ();
+						for (int i =0; i< stars.Length; i++) {
+								stars [i].Pause ();
+						}
+						Vector3 spawnPosition = new Vector3 (Random.Range (-spawnWaves.x, spawnWaves.x), spawnWaves.y, 30);
+						Instantiate (boss, spawnPosition, boss.transform.rotation);
+						bossIstantiate = true;
+				}
+
+
+		}
 }
--- Space44/Assets/Scripts/IAenemy.cs
+++ Space44/Assets/Scripts/IAenemy.cs
@@ -1,81 +1,102 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class IAenemy : MonoBehaviour {
+public class IAenemy : MonoBehaviour
+{
 
 
 
-	public float Life;//Vida do inimigo
-	public float DmgPerColison;//Dano ao colidir 
-	public float Speed;//velocidade de movimento
-	public GameObject Way;//Caminho ate Player
-	Vector3 destiny;//Vector 3 do Caminho
-	public GameObject Target;//Player marcado como alvo de tiros
-	public GameObject Aim;//Mira do inimigo(usado no W.InTarget)
-	Vector3 Bullet;//Vector 3 do Target
-	public ParticleSystem Tiro;//Particulas de tiros
-	private float cooldown = 2.0f;//cooldown de tiro pra outro
-	private float nextFire; //tempo para o proximo tiro
-	public enum E{Foward,JustGo,Follower};//enum de tipos de inimigos(Vai pra frente,vai no player ao ve-lo,segue o player ate bem perto)
-	public E enemy = E.Foward;//Definindo padrao com Foward
-	public enum W{Foward,InTarget};//Defini o comportamento do tiro(Pra frente em Z,e na direçao em que player estiver)
-	public W weapon = W.Foward;//Definindo padrao com Foward
+		public float Life;//Vida do inimigo
+		public float DmgPerColison;//Dano ao colidir 
+		public float Speed;//velocidade de movimento
+		public GameObject Way;//Caminho ate Player
+		Vector3 destiny;//Vector 3 do Caminho
+		public GameObject Target;//Player marcado como alvo de tiros
+		public GameObject Aim;//Mira do inimigo(usado no W.InTarget)
+		Vector3 Bullet;//Vector 3 do Target
+		public ParticleSystem Tiro;//Particulas de tiros
+		private float cooldown = 2.0f;//cooldown de tiro pra outro
+		private float nextFire; //tempo para o proximo tiro
+		public enum E
+		{
+				Foward,
+				JustGo,
+				Follower}
+		;//enum de tipos de inimigos(Vai pra frente,vai no player ao ve-lo,segue o player ate bem perto)
+		public E enemy = E.Foward;//Definindo padrao com Foward
+		public enum W
+		{
+				Foward,
+				InTarget}
+		;//Defini o comportamento do tiro(Pra frente em Z,e na direçao em que player estiver)
+		public W weapon = W.Foward;//Definindo padrao com Foward
 
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start ()
+		{
+				if (GameObject.FindGameObjectWithTag ("Player")) {
+						Target = GameObject.FindGameObjectWithTag ("Player");
+						Way = GameObject.FindGameObjectWithTag ("Player");
+				}
 
-		Target = GameObject.FindGameObjectWithTag("Player");
-		Way =  GameObject.FindGameObjectWithTag("Player");
+				if (enemy == E.JustGo) {
+						Speed = 0.1f;
+						if (Way != null) {
+								destiny = Way.transform.position;
+								transform.LookAt (destiny);
+						}
+				}
+				if (enemy == E.Foward) {
+						Speed = 0.06f;
+				}
+				if (enemy == E.Follower) {
+						Speed = 0.1f;
 
-		if(enemy == E.JustGo){
-			Speed = 0.1f;
-			destiny = Way.transform.position;
-			transform.LookAt (destiny);
-		}
-		if(enemy == E.Foward){
-			Speed = 0.06f;
-		}
-		if(enemy == E.Follower){
-			Speed = 0.1f;
-
+				}
+	
 		}
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		if(enemy == E.JustGo){
-			transform.Translate (new Vector3 (0, 0, Speed));
-
-		}
-		if(enemy == E.Foward){		
-			transform.Translate (new Vector3 (0, 0, Speed));
-
-		}
-		if(enemy == E.Follower){
-			destiny = Way.transform.position;
-			if (transform.position.z > destiny.z + 2.5f) {
-				transform.LookAt (destiny);
-				transform.Translate (new Vector3 (0, 0, Speed));
+		// Update is called once per frame
+		void Update ()
+		{
 				
-			} else {
-				transform.Translate (new Vector3 (0, 0, Speed));
+				if (enemy == E.JustGo) {
+						transform.Translate (new Vector3 (0, 0, Speed));
+
+				}
+				if (enemy == E.Foward) {		
+						transform.Translate (new Vector3 (0, 0, Speed));
+
+				}
+				if (enemy == E.Follower) {
+						if (Way != null) {
+								destiny = Way.transform.position;
+						}
+						if (transform.position.z > destiny.z + 2.5f) {
+								transform.LookAt (destiny);
+								transform.Translate (new Vector3 (0, 0, Speed));
 				
-			}
+						} else {
+								transform.Translate (new Vector3 (0, 0, Speed));
+				
+						}
 			
-		}
-		if(weapon == W.InTarget){
-		Bullet = Target.transform.position;
-		Aim.transform.LookAt(Bullet);
-		}
-		//Tiro na direçao em que nave olhar
-		//Nao se faz nada
-		if(Time.time >  nextFire &&  transform.position.z > Target.transform.position.z ){	
-			Tiro.Emit(1);
-			nextFire = Time.time + cooldown;
+				}
+				if (weapon == W.InTarget) {
+						if (Target != null) {
+								Bullet = Target.transform.position;
+			
+								Aim.transform.LookAt (Bullet);
+						}
+				}
+				//Tiro na direçao em que nave olhar
+				//Nao se faz nada
+				if (Time.time > nextFire && Target != null && transform.position.z > Target.transform.position.z) {	
+						Tiro.Emit (1);
+						nextFire = Time.time + cooldown;
+				}
 		}
 	
-	}
+		
 }
 
