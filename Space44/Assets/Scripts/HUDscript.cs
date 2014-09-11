0a3f2f436c4545c27101b979d059feebedbb8a26
Daniel Castro Martins <daniel_martins070@hotmail.com>
2014-09-11 06:01:55 -0300

    Hud zzz
    ....

--- Space44/Assets/Scripts/HUDscript.cs
+++ Space44/Assets/Scripts/HUDscript.cs
@@ -4,27 +4,40 @@ using System.Collections;
 public class HUDscript : MonoBehaviour {
 	public float MaxLife =100;
 	public float CurrentLife =100;
+	public float MaxSpecial =100;
+	public float CurrentSpecial =100;
+	public float MaxShield =100;
+	public float CurrentShield =100;
+	
 	public float tester;
+	public float width;
 	// Use this for initialization
 	void Start () {
-	
+		
+		width = Screen.width;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		tester = 120/(MaxLife/CurrentLife);
-	
+		
+		width = Screen.width;
+		//Pegar Status do Jogador e setar nas tretas
+		
 	}
 	void OnGUI(){
-	
-		GUI.Box(new Rect(930,10,120,20),""+CurrentLife);
-		GUI.Box(new Rect(930,10,120/(MaxLife/CurrentLife),20),"");
-
-		GUI.Box(new Rect(930,40,120,20),""+CurrentLife);
-		GUI.Box(new Rect(930,40,120/(MaxLife/CurrentLife),20),"");
-
-		GUI.Box(new Rect(930,70,120,20),""+CurrentLife);
-		GUI.Box(new Rect(930,70,120/(MaxLife/CurrentLife),20),"");
-
+		GUI.color = Color.white;
+		//Vida
+		GUI.Box(new Rect(0.86f*width,10,0.13f*width,20),""+CurrentLife);
+		GUI.Box(new Rect(0.86f*width,10,(0.13f*width)/(MaxLife/CurrentLife),20),"");
+		//Laser
+		GUI.Box(new Rect(0.86f*width,35,0.13f*width,20),""+CurrentSpecial);
+		GUI.Box(new Rect(0.86f*width,35,(0.13f*width)/(MaxSpecial/CurrentSpecial),20),"");
+		//Escudo
+		GUI.Box(new Rect(0.86f*width,60,0.13f*width,20),""+CurrentShield);
+		GUI.Box(new Rect(0.86f*width,60,(0.13f*width)/(MaxShield/CurrentShield),20),"");
+		
+		
 	}
 }
