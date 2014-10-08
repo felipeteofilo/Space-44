064dcf54d339afb2b3a82c659a14a1bf49c2ce49
felipeteofilo <felipe.teofilo@hotmail.com>
2014-10-07 22:17:01 -0300

    Fazendo o menu

---
+++ Space44/Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public TextMesh titulo;
+	public TextMesh newGame;
+	public TextMesh load;
+	public TextMesh credits;
+	public TextMesh exit;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
