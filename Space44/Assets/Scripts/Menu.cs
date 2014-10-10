e2d43193f04a1da2602f7a60a60a250ce5d74f83
felipeteofilo <felipe.teofilo@hotmail.com>
2014-10-10 19:54:50 -0300

    Arrumando o menu

--- Space44/Assets/Scripts/Menu.cs
+++ Space44/Assets/Scripts/Menu.cs
@@ -1,25 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Menu : MonoBehaviour {
+public class Menu : MonoBehaviour
+{
 
-	public TextMesh titulo;
-	public TextMesh newGame;
-	public TextMesh load;
-	public TextMesh credits;
-	public TextMesh exit;
-
+		public TextMesh[] buttons;
+		public int menuChose;
+		
 
 
-	// Use this for initialization
-	void Start () {
+
+		// Use this for initialization
+		void Start ()
+		{
 
 
 	
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
+
+				if (Input.GetKeyDown (KeyCode.DownArrow)) {
+						menuChose += 1;
+						if (menuChose > 3) {
+								menuChose = 0;
+						}
+				} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+						menuChose -= 1;
+						if (menuChose < 0) {
+								menuChose = buttons.Length - 1;
+						}
+				}
+
+
+				for (int i = 0; i < buttons.Length; i++) {
+						if (menuChose == i) {
+								buttons [i].renderer.material.color = Color.black;
+						} else {
+								buttons [i].renderer.material.color = Color.white;
+						}
+				}
+				if (Input.GetKeyDown (KeyCode.Return)) {
+						ChoseOption ();
+				}
 	
-	}
+		}
+
+		public void ChoseOption ()
+		{
+
+				switch (menuChose) {
+				case 0:
+						Debug.Log ("new game");
+						break;
+				case 1:
+						Debug.Log ("load");
+						break;
+				case 2:
+						Debug.Log ("credits");
+						break;
+				case 3:
+						Debug.Log ("exit");
+						break;
+
+				}
+		}
 }
---
+++ Space44/Assets/Scripts/NewGameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGameController : MonoBehaviour
+{
+
+		public GameObject[] spaceShips;
+		private int spaceShipChose;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+
+				if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+						spaceShipChose += 1;
+						if (spaceShipChose >= spaceShips.Length) {
+								spaceShipChose = spaceShips.Length - 1;
+						}
+
+				}
+				if (Input.GetKeyDown (KeyCode.RightArrow)) {
+						spaceShipChose -= 1;
+						if (spaceShipChose < 0) {
+								spaceShipChose = 0;
+						}
+				}
+	}
+}
