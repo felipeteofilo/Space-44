d005783ea468779ff81cc5c8a754259dc0e44ffc
felipeteofilo <felipe.teofilo@hotmail.com>
2014-10-16 18:13:18 -0300

    Tentando colocar uma barra descente

---
+++ Space44/Assets/Scripts/BarStatus.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarStatus : MonoBehaviour {
+	public GameObject background;
+
+
+	
+	void Update(){
+
+		background.transform.localScale = new Vector3 (1, 1, 1);
+	}
+}
