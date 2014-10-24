428feb8ba25d7d2d84df85e9c4bd09fc690af43a
Daniel Castro Martins <daniel_martins070@hotmail.com>
2014-10-24 16:10:00 -0200

    lets the rush continue part 3
    ajeitando players e outras tretas

--- Space44/Assets/Scripts/GameController.cs
+++ Space44/Assets/Scripts/GameController.cs
@@ -81,7 +81,7 @@ public class GameController : MonoBehaviour
 		void FixedUpdate ()
 		{
 				if (background.transform.localPosition.z > 2.6f) {
-						//background.transform.Translate (background.transform.forward * -0.05f);
+						background.transform.Translate (background.transform.forward * -0.05f);
 						//planetas.transform.Translate (background.transform.forward * -0.025f);
 				} else if (!bossIstantiate) {
 						audio.Stop ();
